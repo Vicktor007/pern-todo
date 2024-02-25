c2de5992345f8cb00a1ee0f42e60886decb26a4f
Vicktor007 <vicktord007@gmail.com>
2024-02-25 08:22:33 +0100

    save

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "pg13cd",
+    password: process.env.POSTGRE_PASSWORD,
     host: "localhost",
     port: 5432,
     database: "perntodo"
